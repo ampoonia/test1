8985f4a34f6586332c67875bd2ead8b9256ef517
Amar Poonia <ampoonia@hotmail.com>
2015-03-29 10:26:40 +0530

    Added index.php

--- 1.php
+++ 1.php
@@ -1,12 +1,12 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Test file from git.</title>
-<meta charset='utf-8'/>
+	<title>Test file from git.</title>
+	<meta charset='utf-8'/>
 </head>
 <body>
-<h3>Testing Git php..</h3>
-<hr/>
-<?php echo "Just another test..."; ?>
+	<h3>Testing Git php..</h3>
+	<hr/>
+	<?php echo "Just another test..."; ?>
 </body>
 </html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello Git..</title>
+	<meta charset='utf-8'/>
+	<style type="text/css">
+	h3 {
+		text-align: center;
+	}
+	</style>
+</head>
+<body>
+<h3>Testing Git repo.</h3>
+<hr/>
+<?php
+# This is just a testing of git and php
+echo "Hello user";
+?>
+</body>
+</html>
